09cd16fa0e77d9f899b20866bbf3aee9caa62a65
Yevgeniy <y.chaban@tutanota.de>
2021-02-22 00:59:35 +0300

    Transform to simple static site generator

--- gemini2html.go
+++ gemini2html.go
@@ -5,6 +5,9 @@ import( "bufio"
 	"log"
 	"strings"
 	"strconv"
+	"path/filepath"
+	"io"
+	"fmt"
 )
 
 
@@ -81,6 +84,11 @@ func toLi(str string) string {
 	return  "<li>" + trim(str)[2:] + "</li>"
 }
 
+func isVerb(str string) bool {
+	return len(trim(str)) >= 3 && trim(str)[0:3] == "```"
+}
+
+// Did not exist in gemini specs, just for my personal use
 func isImage(str string) bool {
 	return len(trim(str)) > 3 &&  trim(str)[0:3] == "=] "
 }
@@ -105,7 +113,7 @@ func parseFile(filename string, writer *bufio.Writer) {
 	for scanner.Scan() {
 		r := scanner.Text()
 
-		if r == "```" {
+		if isVerb(r) {
 			verb = !verb
 
 			if verb {
@@ -168,12 +176,142 @@ func parseFile(filename string, writer *bufio.Writer) {
 	}
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("No input file provided")
+func file2html(filenameIn, filenameOut string) {
+     fout, err := os.Create(filenameOut)
+     if err != nil {
+     	log.Fatal("Can't create output file " + filenameOut)
+     }
+	 const head = `<head>
+    <meta charset='utf-8'>
+	<style>body{font-family:Open Sans,Arial;color:#454545;font-size:16px;margin:2em auto;max-width:800px;padding:1em;line-height:1.4;text-align:justify}</style>
+    <title>Personal blog</title><meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>`
+     writer := bufio.NewWriter(fout)
+     writer.WriteString("<html>" + head + "<body>")
+     parseFile(filenameIn, writer)
+     writer.WriteString("</body></html>")
+     writer.Flush()
+
+     err = fout.Close()
+     if err != nil {
+     	log.Fatal("Can't close output file")
+     }
+}
+
+func getFileName(filename string) string {
+     idx := strings.LastIndex(filename, ".")
+     if idx == -1 {
+     	return filename
+     }
+
+     return filename[0:idx]
+}
+
+func getFileExt(filename string) string {
+     idx := strings.LastIndex(filename, ".")
+     if idx == -1 {
+     	return filename
+     }
+
+	if idx == len(filename) - 1 {
+		return ""
+    } else {
+    	return filename[idx + 1:]
+	}
+}
+
+func getBaseFile(path string) string {
+		   idx := strings.LastIndex(path, "/")
+		   if idx == -1 {
+		       idx = 0
+		   }
+
+		if idx == len(path) - 1 {
+			return ""
+		}
+
+		return path[idx + 1:]
+}
+
+func getFileNames(dir string) []string {
+
+	fileList := make([]string, 0)
+	e := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+	   	if !f.IsDir() {
+			fileList = append(fileList, path)
+		   }
+		return err
+	})
+
+	if e != nil {
+		log.Fatal("Can't get filenames for Posts directory")
 	}
 
-	writer := bufio.NewWriter(os.Stdout)
-	parseFile(os.Args[1], writer)
-	writer.Flush()
+	return fileList
+}
+
+func copyFile(fSrc, fDst string) int64 {
+
+    src, err := os.Open(fSrc)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer src.Close()
+
+    // Create new file
+    dst, err := os.Create(fDst)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer dst.Close()
+
+    bytesCnt, err := io.Copy(dst, src)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	return bytesCnt
+}
+
+func prepareDirs() {
+	if _, err := os.Stat("_site"); os.IsNotExist(err) {
+     	os.Mkdir("_site", 0777)
+     } else {
+          err := os.RemoveAll("_site")
+
+
+	  if err != nil {
+     	      log.Panic("Can't remove _site directory")
+          }
+
+     os.Mkdir("_site", 0777)
+     }
+
+     os.Mkdir("_site/posts", 0777)
+	 os.Mkdir("_site/assets", 0777)
+	 assets := getFileNames("assets")
+	 for i := range assets {
+		fmt.Printf("Copying file %s into " + "_site/assets/" + getBaseFile(assets[i]), assets[i])
+		bc := copyFile(assets[i], "_site/assets/" + getBaseFile(assets[i]))
+		fmt.Printf(" Done. Copied %d bytes\n", bc)
+	 }
+}
+
+func generateSite() {
+
+	prepareDirs()
+
+     postsFiles := getFileNames("./")
+
+     for i := range postsFiles {
+		baseName := getBaseFile(postsFiles[i])
+		if getFileExt(baseName) == "gmi" {
+     	 	file2html(postsFiles[i], "_site/" + getFileName(postsFiles[i]) + ".html")
+        }
+     }
+
+}
+
+func main() {
+	generateSite()
 }
